326285815aa71097fe93547f43542c1bd822ff43
Martin Vancl <tux.martin@gmail.com>
2016-06-10 13:23:28 +0200

    + vyspis pozadovanych dat do konzole
    Signed-off-by: Martin Vancl <tux.martin@gmail.com>

--- test_server.py
+++ test_server.py
@@ -60,52 +60,72 @@ class Handler(BaseHTTPRequestHandler):
         # https://pythonspot.com/json-encoding-and-decoding-with-python/
         data = json.loads(file_content)
 
-        firmware = data['status']['firmware']['version']
+        firmware = data['board_info']['firmware']
+        board_shortname = data['board_info']['board_shortname']
+
+        pppoe_username = data['pppoe']['username']
+
         uptime = data['status']['host']['uptime']
+
+        for x in data['status']['interfaces']:
+            if x['ifname'] == 'ath0':
+                signal = x['wireless']['signal']
+                rssi = x['wireless']['rssi']
+                noisef = x['wireless']['noisef']
+                ccq = x['wireless']['ccq']
+                txrate = x['wireless']['txrate']
+                rxrate = x['wireless']['rxrate']
+
+                if x['wireless']['polling']['quality']:  # N zarizeni
+                    airmax_quality = x['wireless']['polling']['quality']
+                    airmax_capacity = x['wireless']['polling']['capacity']
 
         for x in data['ifstats']['interfaces']:
             if x['ifname'] == 'ath0':
-                ath0_rx_bytes = x['stats']['rx_bytes']
-            elif x['ifname'] == 'eth0':
-                eth0_tx_bytes = x['stats']['tx_bytes']
+                rx_bytes = x['stats']['rx_bytes']
+                tx_bytes = x['stats']['tx_bytes']
 
-        for y in data['iflist']['interfaces']:
-            if y['ifname'] == 'ath0':
-                mode = y['wireless']['mode']
-                channel = y['wireless']['channel']
-                signal = y['wireless']['signal']
-                essid = y['wireless']['essid']
-                txrate = y['wireless']['txrate']
-                rxrate = y['wireless']['rxrate']
+        for x in data['status']['interfaces']: # IPv4
+            if x['ifname'] == 'br0':
+                if x['ipv4']:
+                    ipv4_addr = x['ipv4']['addr']
+                    ipv4_netmask = x['ipv4']['netmask']
+                    ipv4_broadcast = x['ipv4']['broadcast']
 
-                airmax = y['wireless']['polling']['enabled']
-                airmax_quality = y['wireless']['polling']['quality']
-                airmax_capacity = y['wireless']['polling']['capacity']
-
-            if y['ifname'] == 'br0':
-                ipv4_addr = y['ipv4']['addr']
-                ipv4_netmask = y['ipv4']['netmask']
+        for x in data['status']['interfaces']: # IPv6
+            if x['ifname'] == 'br0':
+                if x['ipv6']:
+                    for y in x['ipv6']:
+                        if y['addr'].startswith('2001'): # doplnit podle IPv6
+                            ipv6_addr = y['addr']
+                            ipv6_plen = y['plen']
 
         print(time.strftime("%d-%m-%Y %H:%M:%S"))
         print "Prijata data: " \
-              "\n\t firmware= %s " \
-              "\n\t uptime= %s " \
-              "\n\t eth0 tx bytes= %s " \
-              "\n\t eth0 ipv4= %s / %s " \
-              "\n\t ath0 rx bytes= %s " \
-              "\n\t ath0 mode= %s " \
-              "\n\t ath0 channel= %s " \
-              "\n\t ath0 signal= %s " \
-              "\n\t ath0 essid= %s " \
-              "\n\t ath0 txrate= %s " \
-              "\n\t ath0 rxrate= %s " \
-              "\n\t ath0 airmax= %s " \
-              "\n\t ath0 airmax_quality= %s " \
-              "\n\t ath0 airmax_capacity= %s " \
-              % (firmware, uptime, eth0_tx_bytes, ipv4_addr, ipv4_netmask, ath0_rx_bytes,
-                 mode, channel, signal, essid, txrate, rxrate, airmax, airmax_quality, airmax_capacity)
-
-        # Begin the response
+              "\n\t 'firmware=%s' " \
+              "\n\t 'board_shortname=%s' " \
+              "\n\t 'pppoe_username=%s' " \
+              "\n\t 'uptime=%s' " \
+              "\n\t 'ath0_signal=%s' " \
+              "\n\t 'ath0_rssi=%s' " \
+              "\n\t 'ath0_noisef=%s' " \
+              "\n\t 'ath0_ccq=%s' " \
+              "\n\t 'ath0_txrate=%s' " \
+              "\n\t 'ath0_rxrate=%s' " \
+              "\n\t 'ath0_rx_bytes=%s' " \
+              "\n\t 'ath0_tx_bytes=%s' " \
+              "\n\t 'br0_ipv4_addr=%s' " \
+              "\n\t 'br0_ipv4_netmask=%s' " \
+              "\n\t 'br0_ipv4_broadcast=%s' " \
+              % (firmware, board_shortname, pppoe_username, uptime, signal, rssi, noisef, ccq, txrate, rxrate, rx_bytes, tx_bytes, ipv4_addr, ipv4_netmask, ipv4_broadcast)
+        if airmax_quality and airmax_capacity:
+            print "\n\t 'ath0_airmax_quality=%s' " \
+                  "\n\t 'ath0_airmax_capacity=%s' " \
+                  % (airmax_quality, airmax_capacity)
+        if ipv6_addr and ipv6_plen:
+            print "\n\t 'br0_ipv6_addr=%s' " \
+                  "\n\t 'br0_ipv6_plen=%s' " \
+                  % (ipv6_addr, ipv6_plen)
         self.send_response(200)
         self.send_header("Connection", "close")
         self.end_headers()
